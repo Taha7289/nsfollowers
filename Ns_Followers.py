4813c687b0406a7ba35d296a2dca0bb73a8f2717
Taha7289 <kifhj48@gmail.com>
2024-05-16 12:13:26 +0500

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='63170433110' #Enter UserID Here
+        self.__username='sigmaboy8292' #Enter UserName Here
+        self._session='63170433110%3A7VX9XcvrvhDEvE%3A15%3AAYf9QbdiYL4EOR4xwzqgBL5K2IuVxZFp3Cwbi7IFWA' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
